5f456edd18e73b62866d3cc43df4917acf5b2235
katerynapidhorna <katerynapidhorna@gmail.com>
2020-09-17 15:21:32 +0200

    add redux thunk to request all products and selector to select and map all products on the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import fetchAllProductsThunk from "../store/products/actions";
+import selectProducts from "../store/products/selectors";
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const products = useSelector(selectProducts);
+
+  useEffect(() => {
+    dispatch(fetchAllProductsThunk());
+  }, [dispatch]);
+
+  console.log(products);
   return (
     <div>
       <h1>Home page</h1>
+      <div>
+        {products &&
+          products.map((item) => {
+            return (
+              <div key={item.id}>
+                <h3>{item.name}</h3>
+                <img src={item.imgUrl} />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
--- src/store/products/reducer.js
+++ src/store/products/reducer.js
@@ -1,7 +1,10 @@
-const initialState = {};
+const initialState = [];
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case "SET_PRODUCTS":
+      return [...state, ...action.payload];
+
     default:
       return state;
   }
