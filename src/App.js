5039b3fe36ffe046914566fccf4249aab7336576
katerynapidhorna <katerynapidhorna@gmail.com>
2020-09-17 14:41:10 +0200

    set redux store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { ApolloProvider } from "react-apollo";
-import { InMemoryCache, HttpLink, ApolloClient, from } from "@apollo/client";
 import { Switch, Route } from "react-router-dom";
-import { ApolloLink } from "@apollo/client";
 import "./App.css";
 import Home from "./components/Home";
 import Navigation from "./components/Navigation/Navigation";
