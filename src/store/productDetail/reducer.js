4df9af69bd7c38a9bc7a831a83679e8c226a744f
Emnetdegafe <emnetdegafe@gmail.com>
2020-10-01 00:11:32 +0200

    productDetail reducer set up /added to rootRed

---
+++ src/store/productDetail/reducer.js
@@ -0,0 +1,11 @@
+const initialState = [];
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_PRODUCT_DETAIL_SUCCESS":
+      return [...state, action.payload];
+
+    default:
+      return state;
+  }
+};
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import products from "./products/reducer";
+import productDetail from "./productDetail/reducer"
 
 export default combineReducers({
   products,
+  productDetail,
 });
